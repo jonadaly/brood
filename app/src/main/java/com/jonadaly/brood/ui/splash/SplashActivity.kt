406a3fa054f167444ba005b216e8e45480da65b9
Jon Daly <jonathan.daly@draysontechnologies.com>
2018-12-25 00:47:48 +0000

    Updated UI

--- app/src/main/java/com/jonadaly/brood/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/jonadaly/brood/ui/splash/SplashActivity.kt
@@ -3,11 +3,14 @@ package com.jonadaly.brood.ui.splash
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.jonadaly.brood.R
 import com.jonadaly.brood.ui.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
         startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
